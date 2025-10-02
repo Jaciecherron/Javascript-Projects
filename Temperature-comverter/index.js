92521e9a42d425a4078056cec467d1cde5a3c0ca
Jaciecherron <jaciecherron@gmail.com>
2025-10-02 10:51:31 -0600

    finished css and started javascript

---
+++ Temperature-comverter/index.js
@@ -0,0 +1,7 @@
+let celsiusE1 = document.getElementById("celsius");
+let fahrenheitE1 = document.getElementById ("fahrenheit");
+let kelvinE1 = document.getElementById("kelvin");
+
+function computeTemp(event) {
+  let currentValue = 
+}
