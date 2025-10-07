e6834fc211abf5c8383fdf1b97c3dc481456e7f4
Jaciecherron <jaciecherron@gmail.com>
2025-10-07 12:08:09 -0600

    Added more css and javascript

--- Heart-trail-animation/index.js
+++ Heart-trail-animation/index.js
@@ -1,8 +1,16 @@
-let bodyE1 = document.querySelector("body")
+const bodyEl = document.querySelector("body");
 
-bodyE1.addEventListener("mousemove", (event) => {
-    let xPos = event.offsetX;
-    let yPos = event.offsetY;
-    let spanE1 = document.createElement("span");
-    bodyE1.appendChild(spanE1);
-})
+bodyEl.addEventListener("mousemove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+  bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
