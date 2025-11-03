772e9993d34d3e0d3256f44541886862693807df
Jaciecherron <jaciecherron@gmail.com>
2025-11-03 15:12:35 -0700

    update javascript

--- dice-roll-simulator/index.js
+++ dice-roll-simulator/index.js
@@ -11,3 +11,33 @@ function rollDice() {
   historyList.push(rollResult);
   updateRollHistory();
 }
+
+function updateRollHistory(){
+  rollHistoryE1.innerHTML = "";
+  for (
+    let i = 0; i < historyList.length; i++
+  ) {
+    let listItem = document.createElement("li");
+  listItem.innerHTML= `Roll ${i + 1}: <span>${getDiceFace(historyList[i])}<span>`;
+  rollHistoryE1.appendChild(listItem);
+}
+}
+
+function getDiceFace (rollResult) {
+  switch (rollResult) {
+    case 1:
+      return "&#9856;";
+    case 2:
+      return "&#9857;";
+    case 3:
+      return "&#9858;";
+    case 4:
+      return "&#9859;";
+    case 5:
+      return "&#9860;";
+    case 6:
+      return "&#9861;";
+    default:
+      return "";
+  }
+}
