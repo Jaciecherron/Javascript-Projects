182551ba21fffa35caaf5b013119c34ede29a4ca
Jaciecherron <jaciecherron@gmail.com>
2025-11-17 14:32:29 -0700

    add javascript

--- Rock-Paper-Scissors-Game/index.js
+++ Rock-Paper-Scissors-Game/index.js
@@ -4,4 +4,14 @@ let resultE1 = document.getElementById("result");
 
 let playerScoreE1 = document.getElementById("user-score");
 
-let computerScoreE1 = document.getElementById
+let computerScoreE1 = document.getElementById("computer-score");
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach ((button) => {
+  button.addEventListener("click", () => {
+    let result = playRound(button.id, computerPlay());
+    resultE1.textContent = result;
+    });
+});
