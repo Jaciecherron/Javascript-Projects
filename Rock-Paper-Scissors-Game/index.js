4c204eaf0348ed857e95b3977a775f9ea1662141
Jaciecherron <jaciecherron@gmail.com>
2025-11-17 13:40:54 -0700

    started javascript

---
+++ Rock-Paper-Scissors-Game/index.js
@@ -0,0 +1,7 @@
+let buttons = document.querySelectorAll("button");
+
+let resultE1 = document.getElementById("result");
+
+let playerScoreE1 = document.getElementById("user-score");
+
+let computerScoreE1 = document.getElementById
