ebbd5ce5c1483f1e573398771ef5d4c50b9be23a
Jaciecherron <jaciecherron@gmail.com>
2025-11-20 10:23:49 -0700

    updated javascript

--- Rock-Paper-Scissors-Game/index.js
+++ Rock-Paper-Scissors-Game/index.js
@@ -21,3 +21,22 @@ function computerPlay() {
   let randomChoice = Math.floor(Math.random() * choices.length );
   return choices[randomChoice];
 }
+
+function playRound (playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  }
+  else if {
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  } {
+    playerScore ++;
+    playerScoreE1.textContent = playerScore;
+    return "You win!" + playerSelection + "beats" + computerSelection;
+  } else {
+    computerScore++;
+    computerScoreE1.textContent = computerScore;
+    return "You lose" + computerSelection + "beats" = playerSelection;
+  }
+}
